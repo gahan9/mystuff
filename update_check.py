54b706d43e8858af6772547ee2b591b2a0e4074c
Gahan9 <gahansaraiya@gmail.com>
2017-05-11 16:06:00 +0530

    grab only corresponding category files.. for first page only

--- update_check.py
+++ update_check.py
@@ -66,63 +66,63 @@ def main(base_url):
             category_url = category_url[0]  # <type 'str'>
         else:
             pass
-        category_dict[str(category_name)] = str(category_url)
-    for categories in category_dict:
-        print('working.', end='')
-        categories_name = categories
-        categories_url = category_dict[categories]
+        if category_name == 'Video':  # only scrap specific addon type
+            category_dict[str(category_name)] = str(category_url)
+    # for categories in category_dict:
+    print('working.', end='')
+    categories_name = 'Video'
+    categories_url = category_dict[categories_name]
 
-        internal_page = OpenPage(categories_url)  # <class 'bs4.BeautifulSoup'>
-        sub_page2 = LoadTag(internal_page, 'ul', 'addonList')  # <class 'bs4.element.ResultSet'>
-        for items in sub_page2:
+    internal_page = OpenPage(categories_url)  # <class 'bs4.BeautifulSoup'>
+    sub_page2 = LoadTag(internal_page, 'ul', 'addonList')  # <class 'bs4.element.ResultSet'>
+    for items in sub_page2:
+        print('.', end='')
+        for plugins in items.findAll('li'):
+            plugin_dictionary = {'kind': categories_name, 'url': categories_url}
             print('.', end='')
-            for plugins in items.findAll('li'):
-                plugin_dictionary = {'kind': categories_name, 'url': categories_url}
-                print('.', end='')
-                plugin_name = LoadUrlList(plugins, 'img', 'alt')  # <type 'list'>
-                plugin_url = LoadUrlList(plugins, 'a', 'href')  # <type 'list'>
-                if len(plugin_name) == len(plugin_url) == 1:
-                    plugin_name = plugin_name[0]  # <type 'str'>
-                    plugin_url = plugin_url[0]  # <type 'str'>
-                else:
-                    pass
-                plugin_page = OpenPage(plugin_url)
-                plugin_detail = LoadTag(plugin_page, 'div', 'addonDetail')[0]  # <class 'bs4.element.Tag'>
-                plugin_meta_detail = LoadTag(plugin_detail, 'div', 'addonMetaData')[0]  # <class 'bs4.element.Tag'>
-                try:
-                    version = re.search('Version: (.+?)Released', plugin_meta_detail.text).group(1)
-                except:
-                    version = ''
-                try:
-                    released = re.search('Released: (.+?)Summary', plugin_meta_detail.text).group(1)
-                except:
-                    released = ''
-                try:
-                    summary = re.search('Summary: (.+?)Host', plugin_meta_detail.text).group(1)
-                except:
-                    summary = ''
-                try:
-                    download_link = plugin_meta_detail.find(href=re.compile("\.zip$"))['href']
-                except:
-                    download_link = ''
-                # description = re.search('Kodi Addon Description:(.+?)Kodi Addon Dependencies', plugin_detail.text).group(1)
-                # dependencies = re.search('Kodi Addon Dependencies:(.+?)Kodi Addon Changelog', plugin_detail.text).group(1)
-                plugin_dictionary['plugin_name'] = str(plugin_name)
-                plugin_dictionary['plugin_url'] = str(plugin_url)
-                plugin_dictionary['version'] = str(version)
-                plugin_dictionary['released'] = str(released)
-                plugin_dictionary['summary'] = str(summary)
-                plugin_dictionary['download_link'] = str(download_link)
-                # plugin_dictionary['description'] = description
-                item_list.append(plugin_dictionary)
+            plugin_name = LoadUrlList(plugins, 'img', 'alt')  # <type 'list'>
+            plugin_url = LoadUrlList(plugins, 'a', 'href')  # <type 'list'>
+            if len(plugin_name) == len(plugin_url) == 1:
+                plugin_name = plugin_name[0]  # <type 'str'>
+                plugin_url = plugin_url[0]  # <type 'str'>
+            else:
+                pass
+            plugin_page = OpenPage(plugin_url)
+            plugin_detail = LoadTag(plugin_page, 'div', 'addonDetail')[0]  # <class 'bs4.element.Tag'>
+            plugin_meta_detail = LoadTag(plugin_detail, 'div', 'addonMetaData')[0]  # <class 'bs4.element.Tag'>
+            try:
+                version = re.search('Version: (.+?)Released', plugin_meta_detail.text).group(1)
+            except:
+                version = ''
+            try:
+                released = re.search('Released: (.+?)Summary', plugin_meta_detail.text).group(1)
+            except:
+                released = ''
+            try:
+                summary = re.search('Summary: (.+?)Host', plugin_meta_detail.text).group(1)
+            except:
+                summary = ''
+            try:
+                download_link = plugin_meta_detail.find(href=re.compile("\.zip$"))['href']
+            except:
+                download_link = ''
+            # description = re.search('Kodi Addon Description:(.+?)Kodi Addon Dependencies', plugin_detail.text).group(1)
+            # dependencies = re.search('Kodi Addon Dependencies:(.+?)Kodi Addon Changelog', plugin_detail.text).group(1)
+            plugin_dictionary['plugin_name'] = str(plugin_name)
+            plugin_dictionary['plugin_url'] = str(plugin_url)
+            plugin_dictionary['version'] = str(version)
+            plugin_dictionary['released'] = str(released)
+            plugin_dictionary['summary'] = str(summary)
+            plugin_dictionary['download_link'] = str(download_link)
+            # plugin_dictionary['description'] = description
+            item_list.append(plugin_dictionary)
+            print(plugin_dictionary)
+
         main_dict['items'] = item_list
         print()
-    with open('video_plugin.json', 'w') as fp:
+    with open('video_plugin2.json', 'w') as fp:
         json.dump(main_dict, fp, sort_keys=False, indent=4)
-    # file_output = open('plugin_dictionary.txt', 'w')
-    # file_output.write(str(main_dict))
-    # file_output.close()
-
+        
 
 if __name__ == '__main__':
     start_time = time.clock()
