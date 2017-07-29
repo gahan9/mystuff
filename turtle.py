29d7bb6681301373c55f0a30caf466fe41d9ce72
gahan-quixom <gahan@quixom.com>
2017-07-29 10:53:38 +0530

    turtle_demo

---
+++ turtle.py
@@ -0,0 +1,25 @@
+import turtle
+
+def tree(branchLen,t):
+    if branchLen > 5:
+        t.forward(branchLen)
+        t.right(20)
+        tree(branchLen-15,t)
+        t.left(40)
+        tree(branchLen-15,t)
+        t.right(20)
+        t.backward(branchLen)
+
+def main():
+    t = turtle.Turtle()
+    myWin = turtle.Screen()
+    t.left(90)
+    t.up()
+    t.backward(400)
+    t.down()
+    t.speed(100)
+    t.color("green")
+    tree(150,t)
+    myWin.exitonclick()
+
+main()
