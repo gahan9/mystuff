fb6614aaf3e3d69226297004d9d2cd8cff4ed29a
unknown <gahan@quixom.com>
2017-09-18 15:50:21 +0530

    solution passed 3 out of 10 test cases

--- foobar/level_3-96hrs/2/solution.py
+++ foobar/level_3-96hrs/2/solution.py
@@ -1,46 +1,32 @@
 from fractions import Fraction
 from pprint import pprint
-# import numpy
-
+import operator
 
 loop_probability = lambda x : Fraction(1, 1-x)
 
-paths = []
-probability = None
-def find_paths_from(current_path, d, next_stage_probability):
-    # current_path: [0]
-    # next_stage_probability: {0: [(1, Fraction(1, 2)), (5, Fraction(1, 2))], 1: [(0, Fraction(4, 9)), (3, Fraction(1, 3)), (4, Fraction(2, 9))], 2: [], 3: [], 4: [], 5: []}
-    # d: {0: [1, 5], 1: [0, 3, 4], 2: [], 3: [], 4: [], 5: []}
+def find_paths_from(current_path, d):
     global paths
     future_nodes = d[current_path[-1]]
-    # print("future_nodes {}".format(future_nodes))
     if future_nodes:
-        # for node in future_nodes:
-        #     if node not in current_path:
-        #         if next_stage_probability[node]:
-        #             print(">> from node: {} to {}".format(node, next_stage_probability[node]))
-        #         [find_paths_from(current_path + [node], d, next_stage_probability, probability)]
-        [find_paths_from(current_path + [node], d, next_stage_probability) for node in future_nodes if node not in current_path]
+        [find_paths_from(current_path + [node], d) for node in future_nodes if node not in current_path]
     else:
-        # print(">> {}".format(current_path))
         paths.append(current_path)
 
 
 def answer(m):
-    pprint(m)
+    # pprint(m)
+    global paths
+    paths = []
     terminals = []
     stage_probability = {}
     for rows in range(len(m)):
         if sum(m[rows]) == 0:
             terminals.append(rows)
             stage_probability[rows] = 0
-
-    # a= numpy.array(m)
-    # null_cols = numpy.where(~a.any(axis=0))[0]
     d={}
     stage_probability = {}
-    next_stage_probability = {}
     count =0
+    next_stage_probability={}
     for row in m:
         count2=0
         if count in terminals:
@@ -49,42 +35,67 @@ def answer(m):
             #     print(null_cols)
             # continue
         current_row_denominator = sum(row)
-        next_stage_probability[count] = []
         d[count] = []
         for element in row:
             if element != 0:
-                next_stage_probability[count].append((count2, Fraction(element, current_row_denominator)))
+                if count in next_stage_probability:
+                    next_stage_probability[count].update({count2: Fraction(element, current_row_denominator)})
+                else:
+                    next_stage_probability[count] = ({count2: Fraction(element, current_row_denominator)})
                 d[count].append(count2)
             count2 +=1
         count+=1
 
-    pprint(next_stage_probability)
+    # print(">>",next_stage_probability)
 
-    final_state_probability = []
-    find_paths_from([0], d, next_stage_probability)
+    find_paths_from([0], d)
+    # print(paths)
     for path in paths:
         stage_probability[path[-1]] = 1
+        calculated_path = []
         for x in range(len(path)-1):
-            print(path[x], path[x+1])
-            current_to_next = [y[1] for y in next_stage_probability[path[x]] if y[0] == path[x+1]][0]
-            next_to_current = [y[1] for y in next_stage_probability[path[x+1]] if y[0] == path[x]]
-            # print(next_to_current)
-            if path[x] in d[path[x+1]] and path[x+1] in d[path[x]] and next_to_current:
-                pass
-                stage_probability[path[-1]] = stage_probability[path[-1]] * current_to_next * loop_probability(current_to_next * next_to_current[0])
-                print(stage_probability[path[-1]], current_to_next * next_to_current[0], loop_probability(current_to_next * next_to_current[0]))
-            else:
-                stage_probability[path[-1]] = stage_probability[path[-1]] * current_to_next
+            source, destination = path[x], path[x+1]
+            current_to_next = next_stage_probability[source][destination]
+            # loop_list = [{s: v} for s in sl for v,k in next_stage_probability.items() if s in k] # [{0: 1}, {1: 0}]
+            loop_list = [v for v,k in next_stage_probability.items() if source in k] # [{0: 1}, {1: 0}]
+            stage_probability[path[-1]] = stage_probability[path[-1]] * current_to_next
+            # print(stage_probability[path[-1]])
+            loop_values = []
+            if loop_list:
+                for items in loop_list:
+                    if items in next_stage_probability[source] and source not in calculated_path:
+                        loop_val = next_stage_probability[source][items] * next_stage_probability[items][source]
+                        # print(">>",next_stage_probability[source][items], next_stage_probability[items][source], loop_val, loop_probability(loop_val))
+                        stage_probability[path[-1]] = stage_probability[path[-1]] * loop_probability(loop_val)
+                        calculated_path.append(source)
+                        calculated_path.append(destination)
+            # print(">Path:{} Source:{} Destination:{} \tloop_list: {} \tcurrent_probability: {}".format(path, source, destination, loop_list, stage_probability[path[-1]]))
+
 
     for states in terminals:
         if states not in stage_probability:
             stage_probability[states] = Fraction(0)
 
+    stage_probability = dict(sorted(stage_probability.items(), key=operator.itemgetter(1), reverse=False))
     print(stage_probability)
+    max_denominator = max([v.denominator for k,v in stage_probability.items()])
 
-# print(answer([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]))
+    final_state_probability = []
+    for terminal in stage_probability:
+        current_denominator = stage_probability[terminal].denominator
+        if not  current_denominator == max_denominator:
+            multiplier = max_denominator / current_denominator
+            # print(multiplier)
+        else:
+            multiplier = 1
+        final_state_probability.append(int(stage_probability[terminal].numerator * multiplier))
+        print("terminal {} : {} --> {}".format(terminal, stage_probability[terminal], int(terminal.numerator * multiplier)))
+    final_state_probability.append(max_denominator)
+    return final_state_probability
+
+print(answer([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]))
 print(answer([[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
-print(answer([[0, 1, 0, 0, 0, 1], [4, 0, 2, 3, 2, 1], [9, 1, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
+# print(answer([[0, 1, 0, 0, 0, 1], [4, 0, 2, 3, 2, 1], [9, 1, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
 """
 {0: [(1, Fraction(1, 2)), (5, Fraction(1, 2))],
  1: [(0, Fraction(1, 3)),
