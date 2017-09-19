186663f2e5e4875d925efd9ad638bbb6edfb912d
unknown <gahan@quixom.com>
2017-09-19 18:53:52 +0530

    update

--- foobar/level_3-96hrs/2/solution0.py
+++ foobar/level_3-96hrs/2/solution0.py
@@ -3,19 +3,23 @@ import collections
 
 loop_probability = lambda x : Fraction(1, 1-x)
 
-# def calculate_loop_states(sources, next_stage_probability, loop_states=[], calculated_loop_states=[]):
-#     record = calculated_loop_states
-#     loop_list = []
-#     for source in sources:
-#         if source not in record:
-#             record.append(source)
-#             loop_list = [v for v,k in next_stage_probability.items() if source in k and v in next_stage_probability[source]]
-#     loop_states += loop_list
-#     if not loop_list:
-#         print(loop_states)
-#         return loop_states
-#     else:
-#         return calculate_loop_states(loop_list, next_stage_probability, loop_states, record)
+def calculate_loop_states(sources, next_stage_probability, loop_states=[], calculated_loop_states=[]):
+    # [v for v,k in next_stage_probability.items() if source in k and v in next_stage_probability[source]]
+    record = calculated_loop_states
+    loop_list = {}
+    for source in sources:
+        if not source in record:
+            record.append(source)
+            if source in loop_list:
+                loop_list[source].update([v for v,k in next_stage_probability.items() if source in k and v in next_stage_probability[source]])
+            else:
+                loop_list[source] = [v for v,k in next_stage_probability.items() if source in k and v in next_stage_probability[source]]
+    if not loop_list or source not in loop_list:
+        # print("> ", loop_states, record)
+        return loop_states
+    else:
+        loop_states.append(loop_list)
+        return calculate_loop_states(loop_list[source], next_stage_probability, loop_states, record)
 
 
 def calculate_probability_of_node(matrix, terminals):
@@ -56,10 +60,11 @@ def find_all_paths(next_stage_probability, start, end, path=[]):
 
 
 def answer(m):
-    from pprint import pprint
-    print("matrix is:")
-    pprint(m)
-    global paths
+    # print("***********************")
+    # from pprint import pprint
+    # print("matrix is:")
+    # pprint(m)
+    # global paths
     paths = []
     terminals = []
     stage_probability = {}
@@ -69,12 +74,12 @@ def answer(m):
             stage_probability[rows] = 0
 
     next_stage_probability, d = calculate_probability_of_node(m, terminals)
-    print("next_stage_probability: {}".format(next_stage_probability))
+    # print("next_stage_probability: {}".format(next_stage_probability))
 
     paths_to_terminal = {}
     for terminal in terminals:
         paths_to_terminal[terminal] = find_all_paths(next_stage_probability=next_stage_probability, start=0, end=terminal)
-    print("paths_to_terminal: {}".format(paths_to_terminal))
+    # print("paths_to_terminal: {}".format(paths_to_terminal))
 
     # this is important stage where next_stage_probability is your tree and you need to calculate probability to reach to states in terminals which are values 0 rows in matrix
     terminal_probability = {}
@@ -90,19 +95,23 @@ def answer(m):
                     #################################################
                     # I need this valie of loop_list to be accurate #
                     #################################################
-                    loop_list = [v for v,k in next_stage_probability.items() if source in k and v in next_stage_probability[source]]
-                    # loop_list = calculate_loop_states([source], next_stage_probability)
+                    # loop_list = [v for v,k in next_stage_probability.items() if source in k and v in next_stage_probability[source]]
+                    loop_list = calculate_loop_states([source], next_stage_probability)
                     this_path_probability = this_path_probability * current_to_next
-                    # print(path, (source, destination),loop_list, this_path_probability)
+                    # loop_list = set(loop_list) - {source}
+                    # print(">> ",path, (source, destination),loop_list, this_path_probability)
                     if loop_list:
                         for loop_state in loop_list:
                             # print((source, loop_state))
-                            if not considered_loop.__contains__((source, loop_state)) and not considered_loop.__contains__((loop_state, source)):
-                                # print(Fraction(next_stage_probability[source][loop_state]), next_stage_probability[loop_state][source])
-                                # print(type(next_stage_probability[source][loop_state]), type(next_stage_probability[loop_state][source]))
-                                loop_val = next_stage_probability[source][loop_state] * next_stage_probability[loop_state][source]
-                                this_path_probability = this_path_probability * loop_probability(loop_val)
-                                considered_loop.append((source, loop_state))
+                            for key_state in loop_state:
+                                for next_state in loop_state[key_state]:
+                                    if not considered_loop.__contains__((key_state, next_state)) and not considered_loop.__contains__((next_state, key_state)):
+                                        # loop_val = next_stage_probability[source][loop_state] * next_stage_probability[loop_state][source]
+                                        if key_state in next_stage_probability and next_state in next_stage_probability:
+                                            if next_state in next_stage_probability[key_state] and key_state in next_stage_probability[next_state]:
+                                                loop_val = next_stage_probability[key_state][next_state] * next_stage_probability[next_state][key_state]
+                                                this_path_probability = this_path_probability * loop_probability(loop_val)
+                                                considered_loop.append((key_state, next_state))
                     # print(this_path_probability)
                 probability_for_this_terminal = probability_for_this_terminal + this_path_probability
                 terminal_probability[terminal_state] = probability_for_this_terminal
@@ -127,9 +136,9 @@ def answer(m):
 
 # test cases below
 # print(answer([[0, 2, 1, 0, 0], [0, 0, 0, 3, 4], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]))
-# print(answer([[0, 7, 0, 5], [2, 0, 3, 0], [0, 6, 0, 0], [0, 0, 0, 0]]))
+print(answer([[0, 7, 0, 5], [2, 0, 3, 0], [0, 6, 0, 0], [0, 0, 0, 0]]))
 print(answer([[0, 1, 0, 0, 0, 1], [4, 0, 0, 3, 2, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
-# print(answer([[0, 3, 1, 0, 0, 0], [0, 0, 0, 7, 0, 0], [0, 0, 0, 2, 0, 1], [0, 0, 0, 0, 5, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
-# print(answer([[0, 3, 2, 0, 0, 0], [0, 0, 0, 1, 0, 1], [0, 0, 0, 7, 0, 0], [0, 0, 5, 0, 4, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
-# print(answer([[0, 3, 2, 0, 0, 0], [0, 0, 0, 1, 0, 1], [0, 0, 0, 7, 0, 0], [0, 0, 0, 0, 4, 5], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
-# print(answer([[0, 1, 0, 0, 0, 1], [4, 0, 2, 3, 2, 1], [9, 1, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
+print(answer([[0, 3, 1, 0, 0, 0], [0, 0, 0, 7, 0, 0], [0, 0, 0, 2, 0, 1], [0, 0, 0, 0, 5, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
+print(answer([[0, 3, 2, 0, 0, 0], [0, 0, 0, 1, 0, 1], [0, 0, 0, 7, 0, 0], [0, 0, 5, 0, 4, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
+print(answer([[0, 3, 2, 0, 0, 0], [0, 0, 0, 1, 0, 1], [0, 0, 0, 7, 0, 0], [0, 0, 0, 0, 4, 5], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
+print(answer([[0, 1, 0, 0, 0, 1], [4, 0, 2, 3, 2, 1], [9, 1, 0, 0, 0, 1], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0]]))
