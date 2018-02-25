dfbaf9d8fdf9f92a56ec6eac7f39798d2c96461a
Gahan9 <gahansaraiya@gmail.com>
2018-02-25 14:16:27 +0530

    result set

--- KickStart/2018_Practice_Round/Problem B. Googol String.py
+++ KickStart/2018_Practice_Round/Problem B. Googol String.py
@@ -23,28 +23,54 @@ SN = SN-1 + "0" + switch(reverse(SN-1)).
 
 You need to figure out the Kth character of Sgoogol, where googol = 10100.
 """
+from threading import Thread
 
 
-def switch(arg):
-    return arg.replace("0", "2").replace("1", "0").replace("2", "1")
+class GoogolString(object):
+    def __init__(self):
+        self.string = ""
+
+    def switch(self, arg):
+        return arg.replace("0", "2").replace("1", "0").replace("2", "1")
+
+    def generate_string(self, string=None):
+        if not string:
+            string = self.string
+        return "{}0{}".format(string, self.switch(string[::-1]))
+
+    def demo_str(self, length=7):
+        s = '0'
+        for st in range(1, length):
+            print("{:2} : {:^65}".format(st, s))
+            s = self.generate_string(s)
+
+    def small_input_string(self, max_value=7):
+        c = 0
+        while c < max_value:
+            self.string = self.generate_string()
+            c += 1
+        return self.string
 
 
-def generate_string(string):
-    return "{}0{}".format(string, switch(string[::-1]))
+def string_generator(max_iter=6):
+    result_string = "0"
+    for i in range(1, max_iter):
+        if i == 0:
+            pass
+        elif i == 1:
+            result_string = "0"
+        else:
+            common_set = 2**(i-1)
+            result_string += "0" + result_string[:common_set]
 
-
-def small_input_string(max_value=18):
-    c = 0
-    s = ""
-    while c < max_value:
-        s = generate_string(s)
-        c += 1
-    return s
 
 if __name__ == "__main__":
-    string = small_input_string()  # generated static small input string according to requirements
+    googl = GoogolString()
+    thr = Thread(target=googl.small_input_string, args=(40,))
+    thr.start()
+    # string = small_input_string()  # generated static small input string according to requirements
     t = int(input())
-    for i in range(1, t+1):
+    for i in range(1, t + 1):
         lookup_index = int(input()) - 1
-        result = string[lookup_index]
+        result = googl.string[lookup_index]
         print("Case #{}: {}".format(i, result))
