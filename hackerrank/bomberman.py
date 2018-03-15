9825f29e4c607f8c884e2f5f228addeef6f47cb0
Gahan <gahansaraiya@gmail.com>
2018-03-15 17:36:13 +0530

    cleaned the code

--- hackerrank/bomberman.py
+++ hackerrank/bomberman.py
@@ -18,25 +18,33 @@ class BomberMan(object):
         self.grid = []
 
     def grid_construct(self):
+        """
+        matrix approach of grid construction
+        pros:
+        implementation : easy
+        cons:
+        space complexity: high
+        time to lookup : high
+        :return: integer matrix of given grid of string
+        """
         default_grid_lis = [[0 for c in range(self.col)] for r in range(self.row)]
-        # print(default_grid_lis, self.initial_grid, sep='\n')
         for row in range(len(self.initial_grid)):
             for col in range(len(self.initial_grid[row])):
                 if self.initial_grid[row][col].strip() == 'O':
-                    # print(row, col, self.initial_grid[row][col])
                     default_grid_lis[row][col] = 3
         return default_grid_lis
 
     def plant(self):
+        """ plant the bombs for grid matrix """
         for row in range(self.row):
             for col in range(self.col):
                 if self.grid_list[row][col] == 0:
                     self.grid_list[row][col] = 3
                 elif self.grid_list[row][col] > 0:
                     self.grid_list[row][col] -= 1
-                    # print(row, col, self.grid_list)
 
     def explode(self, row, col):
+        """ explode bombs recursively in grid matrix """
         # print("exploding bomb at {} {}".format(row, col))
         for i in range(-1, 2, 2):
             if 0 < row+i < self.row:
@@ -47,7 +55,6 @@ class BomberMan(object):
                     self.grid_list[row+i][col] = 0
             if 0 < col+i < self.col:
                 if self.grid_list[row][col+i] == 1:
-                    # print(row, col+i)
                     self.grid_list[row][col + i] = 0
                     self.explode(row, col+i)
                 else:
@@ -58,16 +65,20 @@ class BomberMan(object):
             self.grid_list[row-1][col] = 0
         if col - 1 > 0:
             self.grid_list[row][col-1] = 0
-        for i in range(row, self.row):
+        # for i in range(row, self.row+1):
+        i, c = row, col
+        while i < self.row:
             self.grid_list[i][col] = 0
             if i+1 < self.row:
                 if self.grid_list[i+1][col] != 1:
                     break
-        for c in range(col, self.col):
+            i += 1
+        while c < self.col:
             self.grid_list[row][c] = 0
             if c+1 < self.col:
                 if self.grid_list[row][c+1] != 1:
                     break
+            c += 1
 
     def hold_explode(self, flag="explode"):
         for row in range(self.row):
@@ -75,8 +86,7 @@ class BomberMan(object):
                 if self.grid_list[row][col] > 0:
                     self.grid_list[row][col] = self.grid_list[row][col] - 1
                     if self.grid_list[row][col] == 0 and flag == "explode":
-                        self.explode(row, col)
-        # print(self.grid_list)
+                        self.iterative_explode(row, col)
 
     def bombing_sequence(self):
         """
@@ -85,27 +95,19 @@ class BomberMan(object):
             It is guaranteed that no bombs will detonate at this second.
         After one more second, any bombs planted exactly three seconds ago will detonate.
             Here, Bomber man stands back and observes.
-        :return:
         """
         flag = 0
         for i in range(1, self.steps+1):
             if flag == 0:
-                # wait
                 if i == 1:
-                    # self.initial_grid = self.grid_construct()
                     self.grid_list = self.grid_construct()
                     self.hold_explode()
                 else:
                     self.hold_explode()
-                # print("----------- After step {} @{} -----------".format(i, flag))
-                # print(self.grid_list)
                 flag = 1
                 continue
             elif flag == 1:
-                # plant
                 self.plant()
-                # print("----------- After step {} @{} -----------".format(i, flag))
-                # print(self.grid_list)
                 flag = 0
                 continue
         return True
