ddf82a5c4b22dc46b5265fae7c8e714e27648c06
Gahan <gahansaraiya@gmail.com>
2018-04-25 15:53:46 +0530

    solution 2

--- hackerrank/minion_game.py
+++ hackerrank/minion_game.py
@@ -1,37 +1,29 @@
-import re
-
-
 class Game(object):
     def __init__(self, string):
         self.string = string
+        self.string_length = len(self.string)
         self.vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
 
-    def get_all_substrings(self):
-        length = len(self.string)
-        return set([self.string[i:j + 1] for i in range(length) for j in range(i, length)])
-
-    def count_substring(self, sub_string):
-        match = re.findall('(?=' + sub_string + ')', self.string)
-        return len(match)
-
     def count_score(self):
-        sub_strings = self.get_all_substrings()
-        score_dict = {}
-        for sub_str in sub_strings:
-            score = self.count_substring(sub_str)
-            if sub_str.startswith(self.vowels):
-                score_dict['Kevin'] = score_dict.setdefault('Kevin', 0) + score
+        stuart, kevin = 0, 0
+        for i in range(self.string_length):
+            if s[i] in self.vowels:
+                kevin += self.string_length - i
             else:
-                score_dict['Stuart'] = score_dict.setdefault('Stuart', 0) + score
-        return score_dict
+                stuart += self.string_length - i
+        return stuart, kevin
 
 
 def minion_game(string):
     # your code goes here
     game_instance = Game(string)
-    scores = game_instance.count_score()
-    max_ = max(scores, key=lambda x: scores[x])
-    print("{} {}".format(max_, scores[max_]))
+    stuart, kevin = game_instance.count_score()
+    if kevin > stuart:
+        print("Kevin", kevin)
+    elif kevin < stuart:
+        print("Stuart", stuart)
+    else:
+        print("Draw")
 
 
 if __name__ == '__main__':
