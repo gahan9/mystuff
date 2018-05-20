07644bcfd54563d20bf58ef0108803773944b1b7
gahan9 <gahansaraiya@gmail.com>
2018-05-20 14:24:50 +0530

    an alternate attempt to possible solution

--- techgig/codegladiators_2018/semifinal/Flood in the Jungle/solution.py
+++ techgig/codegladiators_2018/semifinal/Flood in the Jungle/solution.py
@@ -12,7 +12,6 @@ class FloodInJungle(object):
         self.non_capable_tree = non_capable_tree
         self.euclidean_dict = self.construct_dict()
         self.meetup_tree = []
-        # print(self.euclidean_dict)
 
     @staticmethod
     def euclidean_distance(coordinate1, coordinate2):
@@ -28,6 +27,18 @@ class FloodInJungle(object):
         return d
 
     def get_paths(self):
+        if len(self.non_capable_tree) == 1:
+            end_tree = self.non_capable_tree[0]
+            idx = self.data[end_tree][2]
+            reach1 = list(self.data.keys())[:idx]
+            reach2 = list(self.data.keys())[idx+1:]
+            reach3 = reach1 + reach2
+            if self.sub_calc(reach3, [end_tree]):
+                return idx
+            elif self.sub_calc(reach1, [end_tree]) and self.sub_calc(reach2, [end_tree]):
+                return idx
+            else:
+                return -1
         for path in self.permutations:
             # print(path, '|', list(map(lambda x: self.data[x], path)))
             p = []
@@ -52,44 +63,19 @@ class FloodInJungle(object):
             if len(p) is self.data_size:
                 return ' '.join(map(str, [self.data[i][2] for i in p]))
                 # paths.append(p)
-        return -1
-
-    def get_path(self, data, size, **kwargs):
-        value = kwargs.get('elem', None)
-        possible_meetup_tree = []
-        for i in permutations(data, size):  # print(path, '|', list(map(lambda x: self.data[x], path)))
-            if value:
-                path = list(i) + value
-            else:
-                path = i
-            print(">> ", path)
-            p = []
-            for nxt in path:  # path[i] = ((1, 10), (5, 10), (8, 10))
-                try:
-                    prev = p[-1]
-                    if self.euclidean_distance(prev, nxt) <= self.threshold:
-                        prev_monkey, prev_limit, prev_idx, capability = self.data[prev]
-                        if prev_monkey <= prev_limit:  # print(">>> ", prev, nxt, self.euclidean_distance(prev, nxt), p)
-                            p.append(nxt)
-                    else:
-                        break
-                except IndexError:
-                    p.append(nxt)
-            print("--* ", p, size)
-            checker = size + 1 if value else size
-            if len(p) is checker:
-                possible_meetup_tree.append(self.data[p[-1]][2])
-        # if paths:  # [[0, 1, 2], [1, 0, 2], [2, 0, 1]]
-        #     return ' '.join(map(str, [self.data[i][2] for i in paths[0]]))
-        if possible_meetup_tree:
-            return sorted(set(possible_meetup_tree))
-        return 0
+        return self.calculate()
 
     def sub_calc(self, dataset, elem):
-        meet_up_trees = []
         size = len(dataset)
+        flag = True
+        for i in self.data.keys():
+            if self.euclidean_distance(i, elem[0]) > self.threshold:
+                flag = False
+        if flag:
+            return flag
         for i in permutations(dataset, size):
             path = list(i) + elem
+            print(path)
             p = []
             monkey_sum = 0
             for nxt in path:
@@ -106,50 +92,10 @@ class FloodInJungle(object):
                             p.append(nxt)
                 except IndexError:
                     p.append(nxt)
+            print(p, size)
             if len(p) is size + 1:
                 return True
-                # meet_up_trees.append(self.data[p[-1]][2])
         return False
-
-    def get_results(self):
-        if len(self.non_capable_tree) == 1:
-            end_tree = self.non_capable_tree[0]
-            idx = self.data[end_tree][2]
-            reach1 = list(self.data.keys())[:idx]
-            reach2 = list(self.data.keys())[idx+1:]
-            reach3 = reach1 + reach2
-            if self.sub_calc(reach3, [end_tree]):
-                return idx
-            elif self.sub_calc(reach1, [end_tree]) and self.sub_calc(reach2, [end_tree]):
-                return idx
-            else:
-                return -1
-        elif len(self.non_capable_tree) < 1:
-            meet_up_trees = []
-            for path in permutations(self.data, self.data_size):
-                print(path)
-                p = []
-                monkey_sum = 0
-                for nxt in path:
-                    try:
-                        prev = p[-1]
-                        if self.euclidean_distance(prev, nxt) > self.threshold:
-                            break
-                        else:
-                            prev_monkey, prev_limit, prev_idx, capability = self.data[prev]
-                            monkey_sum += prev_monkey
-                            if monkey_sum > prev_limit:
-                                break
-                            else:
-                                p.append(nxt)
-                    except IndexError:
-                        p.append(nxt)
-                if len(p) is self.data_size:
-                    meet_up_trees.append(self.data[p[-1]][2])
-            if meet_up_trees:
-                return ' '.join(map(str, meet_up_trees))
-        else:
-            return -1
 
     def find_all_paths(self, graph, start, end, path=[]):
         path = path + [start]
@@ -211,7 +157,7 @@ if __name__ == "__main__":
     if len(non_capable_trees) > 1:
         print("-1")
     else:
-        print(f.calculate())
+        print(f.get_paths())
 
 
 """
@@ -220,16 +166,25 @@ if __name__ == "__main__":
 5 10 5 1
 8 10 5 4
 
+-1
+
+
 3 100.0
 1 10 5 20
 5 10 5 20
 8 10 5 20
+
+0 1 2
+
 
 4 2.0
 1 10 5 5
 3 10 5 5
 5 10 7 5
 7 10 5 20
+
+-1
+
 
 5 10.0
 1 10 5 5  		
@@ -238,10 +193,29 @@ if __name__ == "__main__":
 8 10 0 0
 15 10 0 0
 
+-1
+
 
 4 2.0
 1 10 5 20
 3 10 5 20
 5 10 5 20
+7 10 5 20
+
+0 1 2 3
+
+
+3 100.0
+1 10 5 5
+-50 -10 0 6
+-40 -50 0 5
+
+0 1 2
+
+
+4 2.0
+1 10 5 5
+3 10 5 10
+5 10 7 5
 7 10 5 20
 """
