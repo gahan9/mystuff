d96be81688cb0615e342dcfa749055e7319caf18
gahan9 <gahansaraiya@gmail.com>
2018-08-20 01:21:29 +0530

    graph attempt 1

--- plotly_basic/plot_map.py
+++ plotly_basic/plot_map.py
@@ -16,23 +16,27 @@ colorscale = ["#f7fbff", "#ebf3fb", "#deebf7", "#d2e3f3", "#c6dbef", "#b3d2e9", 
               "#08519c", "#0b4083", "#08306b"]
 color = ["#ff7c00", "#a91f78", "#0432ff", "#00a643", "#a6f900", "#fffb00", "#ffd300", "#c8e000"]
 df_sample = df_sample.loc[df_sample['log10(DATA_SET)'] != 4.69]
+df_algos = {}
 dfs = {}
-dfs['ascending'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' ascending']
-dfs['descending'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' descending']
-dfs['random'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' random']
-dfs['same'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' same']
-
 data = []
-cnt = 0
-for name, df in dfs.items():
-    x = df_sample['log10(DATA_SET)'].tolist()
-    # text = df_sample['ALGORITHM'].tolist()
-    algo = [i.split('_')[0] for i in df_sample['ALGORITHM'].tolist()]
-    y = [i*1000 for i in df_sample['TIME'].tolist()]
-    xx = list('_'.join(map(str, i)) for i in zip(algo, df_sample['DATA_SET_TYPE'].tolist()))
-    ht = list('-'.join(map(str, i)) for i in zip(x, y))
-    data += [go.Scatter(x=x, y=y, text=xx, hovertext=ht, name=name, marker=dict(color=color[cnt]))]
-    cnt += 1
+for algo_name in set(df_sample['ALGORITHM'].tolist()):
+    df_filtered = df_sample.loc[df_sample['ALGORITHM'] == algo_name]
+    df_algos[algo_name] = df_filtered
+    cnt = 0
+    for name, df in dfs.items():
+        x = df_filtered['log10(DATA_SET)'].tolist()
+        # text = df_sample['ALGORITHM'].tolist()
+        algo = [i.split('_')[0] for i in df_filtered['ALGORITHM'].tolist()]
+        y = [i * 1000 for i in df_filtered['TIME'].tolist()]
+        xx = list('_'.join(map(str, i)) for i in zip(algo, df_filtered['DATA_SET_TYPE'].tolist()))
+        ht = list('-'.join(map(str, i)) for i in zip(x, y))
+        data += [go.Scatter(x=x, y=y, text=xx, hovertext=ht, name=name, marker=dict(color=color[cnt]))]
+        cnt += 1
+# dfs['ascending'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' ascending']
+# dfs['descending'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' descending']
+# dfs['random'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' random']
+# dfs['same'] = df_sample.loc[df_sample['DATA_SET_TYPE'] == ' same']
+
 layout = go.Layout(title='titlE', barmode='group',
                    xaxis={'title': 'algorithm and dataset type', 'tickformat': ',d'},
                    yaxis={'title': 'time (ms)', 'tickformat': ',d'})
