f6aa20278b343a36d41acb758f13e9b8981f2978
gahan9 <gahansaraiya@gmail.com>
2018-08-29 20:16:50 +0530

    added example ip address

--- network_static_config_windows10.bat
+++ network_static_config_windows10.bat
@@ -1,12 +1,15 @@
 REM Script must be run as administrator
 REM enter interface name in quote
 set IFACE_NAME="Wi-Fi"
-REM add assigned IP
-set IP_ASSIGNED=
-set GATEWAY=
-set PRIMARY_DNS=
+REM add assigned IP instead of 192.168.0.2
+set IP_ASSIGNED=192.168.0.2
+REM enter gateway here instead of 192.168.0.2
+set GATEWAY=192.168.0.1
+REM Enter subnet mask instead of 255.255.255.0
+set SUBNET_MASK=255.255.255.0
+REM enter/change primary dns instead of 8.8.8.8 if require
+set PRIMARY_DNS=8.8.8.8
 set ALTERNATE_DNS=4.2.2.2
-set SUBNET_MASK=
 REM set ip, dns and gateway
 netsh interface ip set address name=%IFACE_NAME% static %IP_ASSIGNED% %SUBNET_MASK% %GATEWAY%
 REM set primary dns
